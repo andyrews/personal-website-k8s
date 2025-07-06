9deb5d8ede65423cf65f566a8e94989ddf393087
andyrews <john.andrew.gacho@gmail.com>
2025-07-06 10:24:53 +0800

    Final backend with manifests

--- personal-website-backend/src/main/java/com/example/personal_website/WebConfig.java
+++ personal-website-backend/src/main/java/com/example/personal_website/WebConfig.java
@@ -18,10 +18,9 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowedHeaders("*");
             }
         };
     }
